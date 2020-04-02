c62a0b721ae027d5e80a701f33b2350730ea9dd8
Michael Kirk <michael.code@endoftheworl.de>
2020-04-02 14:33:40 -0600

    rename package, package as lib
    includes lib.rs so we can include it as an external crate
    
    some temporary hacks to also expose c impl

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod geodesic;
+pub use geodesic::Geodesic;
+
+mod geodesiccapability;
+pub use geodesiccapability::DISTANCE;
+
+mod geodesicline;
+mod geomath;
+
+// TODO only use this for benchmark
+use geographiclib;
