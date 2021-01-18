0acbd65124eeae0ea44e774d3e617ae57e9022d1
Stony <stony.lohr@gmail.com>
2021-01-17 22:49:49 -0800

    draft reduced GeodTest.dat validation, still adjusting

--- src/geodesic.rs
+++ src/geodesic.rs
@@ -1310,6 +1310,7 @@ impl InverseGeodesic<(f64, f64, f64, f64, f64, f64, f64, f64)> for Geodesic {
 mod tests {
     use super::*;
     use assert_approx_eq::assert_approx_eq;
+    use std::io::BufRead;
 
     #[test]
     fn test_inverse_and_direct() -> Result<(), String> {
@@ -2059,4 +2060,111 @@ mod tests {
             "geod._C4x wrong"
         );
     }
+
+    // *_geodtest_* tests are based on Karney's GeodTest*.dat test datasets.
+    // A description of these files' content can be found at:
+    //     https://geographiclib.sourceforge.io/html/geodesic.html#testgeod
+    // Here are some key excerpts...
+    //    This consists of a set of geodesics for the WGS84 ellipsoid.
+    //     Each line of the test set gives 10 space delimited numbers
+    //         latitude at point 1, lat1 (degrees, exact)
+    //         longitude at point 1, lon1 (degrees, always 0)
+    //         azimuth at point 1, azi1 (clockwise from north in degrees, exact)
+    //         latitude at point 2, lat2 (degrees, accurate to 10−18 deg)
+    //         longitude at point 2, lon2 (degrees, accurate to 10−18 deg)
+    //         azimuth at point 2, azi2 (degrees, accurate to 10−18 deg)
+    //         geodesic distance from point 1 to point 2, s12 (meters, exact)
+    //         arc distance on the auxiliary sphere, a12 (degrees, accurate to 10−18 deg)
+    //         reduced length of the geodesic, m12 (meters, accurate to 0.1 pm)
+    //         the area under the geodesic, S12 (m2, accurate to 1 mm2)
+    // These tests are flagged as "ignore" because they're slow and not self-contained
+    // (since they need to read data files), so they only run if specifically requested.
+
+    fn geodtest_basic<T>(f: T)
+    where T: Fn(usize, &(f64, f64, f64, f64, f64, f64, f64, f64, f64, f64))
+    {
+        let dir_base = std::env::current_dir().expect("Failed to determine current directory");
+        let path_base = dir_base.as_path();
+        let pathbuf = std::path::Path::new(path_base)
+            .join("test_fixtures/test-data-unzipped/GeodTest.dat");
+        let path = pathbuf.as_path();
+        let file = match std::fs::File::open(path) {
+            Ok(val) => val,
+            Err(_error) => {
+                let path_str = path.to_str().expect("Failed to convert GeodTest path to string during error reporting");
+                panic!("Failed to open GeodTest.dat file. It may need to be downloaded and unzipped to: {}\nFor details see https://geographiclib.sourceforge.io/html/geodesic.html#testgeod", path_str)
+            }
+        };
+        let reader = std::io::BufReader::new(file);
+        reader.lines().enumerate()
+            .for_each(|(i, line)| {
+                let line_safe = line.expect("Failed to read line");
+                let items: Vec<f64> = line_safe.split(' ').enumerate()
+                .map(|(j, item)| {
+                    match item.parse::<f64>() {
+                        Ok(parsed) => parsed,
+                        Err(_error) => panic!("Error parsing item {} on line {}: {}", j+1, i+1, item),
+                    }                    
+                })
+                .collect();
+                assert_eq!(items.len(), 10);
+                let tuple = (items[0], items[1], items[2], items[3], items[4], items[5], items[6], items[7], items[8], items[9]);
+                f(i+1, &tuple); // report 1-based line number rather than 0-based
+            });
+    }
+
+    #[test]
+    #[ignore] // Slow. Requires external file.
+    fn test_geodtest_geodesic_direct12() {
+        let g = std::sync::Arc::new(std::sync::Mutex::new(Geodesic::wgs84()));
+        geodtest_basic(|_line_num, &(lat1, lon1, azi1, lat2, lon2, azi2, s12, a12, m12, S12)| {
+            let g = g.lock().unwrap();
+            let (lat2_out, lon2_out, azi2_out, m12_out, _M12_out, _M21_out, S12_out, a12_out) =
+                g.direct(lat1, lon1, azi1, s12);
+            assert_approx_eq!(lat2, lat2_out, 8e-14);
+            assert_approx_eq!(lon2, lon2_out, 2e-8);
+            assert_approx_eq!(azi2, azi2_out, 2e-8);
+            assert_approx_eq!(m12, m12_out, 3e-9);
+            assert_approx_eq!(S12, S12_out, 1e-3);
+            assert_approx_eq!(a12, a12_out, 3e-14);
+        });
+    }
+
+    #[test]
+    #[ignore] // Slow. Requires external file.
+    fn test_geodtest_geodesic_direct21() {
+        let g = std::sync::Arc::new(std::sync::Mutex::new(Geodesic::wgs84()));
+        geodtest_basic(|_line_num, &(lat1, lon1, azi1, lat2, lon2, azi2, s12, a12, m12, S12)| {
+            let g = g.lock().unwrap();
+            let (lat1, lon1, azi1, lat2, lon2, azi2, s12, a12, m12, S12) =
+                (lat2, lon2, azi2, lat1, lon1, azi1, -s12, -a12, m12, S12);
+            let (lat2_out, lon2_out, azi2_out, m12_out, _M12_out, _M21_out, S12_out, a12_out) =
+                g.direct(lat1, lon1, azi1, s12);
+            assert_approx_eq!(lat2, lat2_out, 8e-14);
+            assert_approx_eq!(lon2, lon2_out, 2e-8);
+            assert_approx_eq!(azi2, azi2_out, 2e-8);
+            assert_approx_eq!(m12, m12_out, 3e-9);
+            assert_approx_eq!(S12, S12_out, 1e-3);
+            assert_approx_eq!(a12, a12_out, 3e-14);
+        });
+    }
+
+    #[test]
+    #[ignore] // Slow. Requires external file.
+    fn test_geodtest_geodesic_inverse12() {
+        let g = std::sync::Arc::new(std::sync::Mutex::new(Geodesic::wgs84()));
+        geodtest_basic(|_line_num, &(lat1, lon1, azi1, lat2, lon2, azi2, s12, a12, m12, S12)| {
+            let g = g.lock().unwrap();
+            let (s12_out, azi1_out, azi2_out, m12_out, _M12_out, _M21_out, S12_out, a12_out) =
+                g.inverse(lat1, lon1, lat2, lon2);
+            assert_approx_eq!(s12, s12_out, 4e-9);
+            assert_approx_eq!(azi1, azi1_out, 2e-2);
+            assert_approx_eq!(azi2, azi2_out, 2e-2);
+            assert_approx_eq!(m12, m12_out, 3e-6);
+            assert_approx_eq!(S12, S12_out, 2e7); // Note: Tolerance not even remotely reasonable
+            assert_approx_eq!(a12, a12_out, 2e-14);
+        });
+    }
+
+
 }
