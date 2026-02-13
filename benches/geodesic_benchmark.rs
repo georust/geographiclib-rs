6e321b2b1b31863cc7830e926ab7417d198c4fea
Michael Kirk <michael.code@endoftheworl.de>
2026-02-13 09:31:53 -0800

    bench polygon area

--- benches/geodesic_benchmark.rs
+++ benches/geodesic_benchmark.rs
@@ -5,7 +5,7 @@ extern crate geographiclib_rs;
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::time::Duration;
 
-use geographiclib_rs::{DirectGeodesic, InverseGeodesic};
+use geographiclib_rs::{DirectGeodesic, InverseGeodesic, PolygonArea, Winding};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -120,9 +120,60 @@ fn geodesic_inverse_benchmark(c: &mut Criterion) {
     }
 }
 
+fn polygon_area_benchmark(c: &mut Criterion) {
+    let geojson_str = std::fs::read_to_string("test_fixtures/countries.geojson").unwrap();
+    let geojson: geojson::GeoJson = geojson_str.parse().unwrap();
+    let features = match geojson {
+        geojson::GeoJson::FeatureCollection(fc) => fc.features,
+        _ => panic!("expected FeatureCollection"),
+    };
+
+    // Extract all polygon rings as Vec<Vec<(lat, lon)>>
+    let mut rings: Vec<Vec<(f64, f64)>> = Vec::new();
+    for feature in &features {
+        let geometry = feature.geometry.as_ref().unwrap();
+        match &geometry.value {
+            geojson::Value::Polygon(coords) => {
+                for ring in coords {
+                    rings.push(ring.iter().map(|c| (c[1], c[0])).collect());
+                }
+            }
+            geojson::Value::MultiPolygon(polys) => {
+                for polygon in polys {
+                    for ring in polygon {
+                        rings.push(ring.iter().map(|c| (c[1], c[0])).collect());
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    let mut group = c.benchmark_group("polygon_area");
+    group.bench_function("countries", |b| {
+        let geod = geographiclib_rs::Geodesic::wgs84();
+        b.iter(|| {
+            let mut area_sum = 0.0;
+            for ring in &rings {
+                let mut pa = PolygonArea::new(&geod, Winding::Clockwise);
+                for &(lat, lon) in ring {
+                    pa.add_point(lat, lon);
+                }
+                let (_perimeter, area, _point_count) = pa.compute(true);
+                area_sum += area;
+            }
+            // Sanity check, some light googling confirms the land on earth is indeed about 150_000_000_000_000
+            // so this is order of magnitude correct.
+            assert_eq!(area_sum.round(), 147_362_836_335_330.0);
+        })
+    });
+    group.finish();
+}
+
 criterion_group!(
     benches,
     geodesic_direct_benchmark,
-    geodesic_inverse_benchmark
+    geodesic_inverse_benchmark,
+    polygon_area_benchmark,
 );
 criterion_main!(benches);
